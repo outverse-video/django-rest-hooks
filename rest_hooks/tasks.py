8e8ff2d9dbfc0ddfc3801b034468457acbc47898
Fergus Leahy <ferg@outverse.com>
2022-08-17 15:43:13 +0100

    update to support Celery >=5.0

--- rest_hooks/tasks.py
+++ rest_hooks/tasks.py
@@ -1,35 +1,46 @@
-import requests
 import json
 
-from celery.task import Task
-
+import requests
 from django.core.serializers.json import DjangoJSONEncoder
-
 from rest_hooks.utils import get_hook_model
 
 
-class DeliverHook(Task):
-    def run(self, target, payload, instance=None, hook_id=None, **kwargs):
-        """
-        target:     the url to receive the payload.
-        payload:    a python primitive data structure
-        instance:   a possibly null "trigger" instance
-        hook:       the defining Hook object (useful for removing)
-        """
-        response = requests.post(
-            url=target,
-            data=json.dumps(payload, cls=DjangoJSONEncoder),
-            headers={'Content-Type': 'application/json'}
-        )
+def _do_hook(target, payload, instance=None, hook_id=None, **kwargs):
+    """
+    target:     the url to receive the payload.
+    payload:    a python primitive data structure
+    instance:   a possibly null "trigger" instance
+    hook:       the defining Hook object (useful for removing)
+    """
+    response = requests.post(
+        url=target,
+        data=json.dumps(payload, cls=DjangoJSONEncoder),
+        headers={"Content-Type": "application/json"},
+    )
 
-        if response.status_code == 410 and hook_id:
-            HookModel = get_hook_model()
-            hook = HookModel.object.get(id=hook_id)
-            hook.delete()
+    if response.status_code == 410 and hook_id:
+        HookModel = get_hook_model()
+        hook = HookModel.object.get(id=hook_id)
+        hook.delete()
 
-        # would be nice to log this, at least for a little while...
+    # would be nice to log this, at least for a little while...
 
-def deliver_hook_wrapper(target, payload, instance=None, hook=None, **kwargs):
-    if hook:
-        kwargs['hook_id'] = hook.id
-    return DeliverHook.delay(target, payload, **kwargs)
+
+try:
+    # Celery < 5.0 backwards compatibility
+    from celery.task import Task
+
+    class DeliverHook(Task):
+        def run(self, target, payload, instance=None, hook_id=None, **kwargs):
+            _do_hook(target, payload, instance=instance, hook_id=hook_id, **kwargs)
+
+except ImportError:
+    from celery import shared_task
+
+    @shared_task
+    class DeliverHook:
+        def __init__():
+            pass
+
+        def __call__(self, target, payload, instance=None, hook_id=None, **kwargs):
+            _do_hook(target, payload, instance=instance, hook_id=hook_id, **kwargs)
